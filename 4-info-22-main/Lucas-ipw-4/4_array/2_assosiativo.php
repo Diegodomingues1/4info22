f2e9893a9daaf96a45a208207722c64434d9bc5d
Diegodomingues1 <108497301+Diegodomingues1@users.noreply.github.com>
2022-07-07 21:09:00 -0300

    Add files via upload

---
+++ 4-info-22-main/Lucas-ipw-4/4_array/2_assosiativo.php
@@ -0,0 +1,24 @@
+<?php
+$pessoa = ARRAY ();
+
+
+$pessoa['nome']='diego';
+$pessoa['email']='diego.carlo@escola.pr.gov.br';
+$pessoa['telefone']='449971398';
+
+
+echo $pessoa['nome'];
+
+echo '<br>';
+
+echo $pessoa['email'];
+
+echo '<br>';
+
+echo $pessoa['telefone'];
+
+
+
+
+?>
+
---
+++ 4-info-22-main/README.md
@@ -0,0 +1,3 @@
+# 4-info-22
+Arquivos das aulas de IPW e banco de dados
+30/06/2022
